ec6621bc49b539eb35f71eab06bfc08255325fa1
PriyaRanjan Kumar <mishrasubham00@gmail.com>
2021-03-01 00:30:01 +0530

    add find user by id

--- server.js
+++ server.js
@@ -15,11 +15,48 @@ app.listen(port, () => {
 // CREATE
 app.post('/users', (req, res) => {
   // User.create()
+
+  User.create({
+    name: req.body.newUser.name,
+    email: req.body.newUser.email,
+    password: req.body.newUser.password
+  }, (err, data) => {
+    if (err) {
+      res.json({ success: false, message: err })
+    }
+    else if (!data) {
+      res.json({ success: false, message: "Not Found" })
+    }
+    else {
+      res.json({ success: true, message: data })
+    }
+  }
+  )
 })
 
 app.route('/users/:id')
   // READ
   .get((req, res) => {
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        })
+      }
+      else if (!data) {
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      }
+      else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    })
     // User.findById()
   })
   // UPDATE
